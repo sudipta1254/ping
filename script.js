117dae15175ce1dfccc14ffc75aaa46acf36a379
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-05-16 10:27:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,32 +27,6 @@ fetch('https://api.weatherapi.com/v1/current.json?q='+q+'&key=df1745f8c6cc4466bf
   }); 
 
 function as() {
-
-document.querySelector('.s1').innerHTML = loc.name+', '+loc.region+', '+loc.country;
-
-document.querySelector('.s2').innerHTML = loc.localtime;
-
-document.querySelector('.s3').innerHTML = cur.last_updated;
-
-document.querySelector('.s4').innerHTML = cur.temp_c+'°C';
-
-document.querySelector('.s5').innerHTML = cur.condition.text;
-
-document.querySelector('.s6').innerHTML = cur.feelslike_c+'°C';
-
-document.querySelector('.s7').innerHTML = cur.wind_kph+' KMPH('+cur.wind_degree+'° -- '+cur.wind_dir+')';
-
-document.querySelector('.s8').innerHTML = cur.gust_kph+' KHPH';
-
-document.querySelector('.s9').innerHTML = cur.pressure_mb+' hPa';
-
-document.querySelector('.s10').innerHTML = cur.humidity+'%';
-
-document.querySelector('.s11').innerHTML = cur.cloud+'%';
-
-document.querySelector('.s12').innerHTML = cur.vis_km+' Km';
-  }); 
-function as() {
 document.querySelector('.s1').innerHTML = loc.name+', '+loc.region+', '+loc.country;
 document.querySelector('.s2').innerHTML = loc.localtime;
 document.querySelector('.s3').innerHTML = cur.last_updated;
