0b8835fbf0ef4910b73da5b8b4f7729978f589d8
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-05-21 21:11:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,9 @@ fetch('https://api.weatherapi.com/v1/current.json?q='+q+'&key=df1745f8c6cc4466bf
 }
 
 function as() {
-  time();
    document.querySelector('.s1').innerHTML = loc.name+', '+loc.region+', '+loc.country;
    document.querySelector('.s2').innerHTML = loc.localtime;
-   document.querySelector('.s3').innerHTML = cur.last_updated+' / '+c+' minutes ago';
+   document.querySelector('.s3').innerHTML = cur.last_updated+' / '+time()+' minutes ago';
    document.querySelector('.s4').innerHTML = cur.temp_c+'°C';
    document.querySelector('.s5').innerHTML = cur.condition.text;
    document.querySelector('.s6').innerHTML = cur.feelslike_c+'°C';
@@ -45,10 +44,10 @@ function get() {
 
 function time() {
    s = loc.localtime.split(' ')[1].split(':');
-   s = +s[0] + +s[1];
+   s = s[0] + s[1];
    c = cur.last_updated.split(' ')[1].split(':');
-   c = +c[0] + +c[1];
-   c = s - c;
+   c = c[0] + c[1];
+   return +s - (+c);
 }
 m();
   
